9187c65c92dc0d9cab922931f98304eb065afc6c
qisarazu <qisarazu319@gmail.com>
2022-01-26 06:27:41 +0900

    Add fonts

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  printWidth: 120,
+  tab: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+};
---
+++ pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;700&family=Nunito:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
