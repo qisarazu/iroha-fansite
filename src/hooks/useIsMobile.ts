f1b92610f7e7b39fa693daa0650073dd83ca0f5b
qisarazu <qisarazu319@gmail.com>
2023-03-12 12:01:53 +0900

    Fix: isMobile error

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -1,8 +1,22 @@
-import { useMemo } from 'react';
-import { useWindowSize } from 'react-use';
+import { useEffect, useMemo, useState } from 'react';
 
 export function useIsMobile() {
-  const { width } = useWindowSize();
+  const [width, setWidth] = useState<number>(Infinity);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setWidth(window.innerWidth);
+
+      const listener = () => {
+        setWidth(window.innerWidth);
+      };
+      window.addEventListener('resize', listener);
+
+      return () => {
+        window.removeEventListener('resize', listener);
+      };
+    }
+  }, []);
 
   return useMemo(() => width <= 768, [width]);
 }
