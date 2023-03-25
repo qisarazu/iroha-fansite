381bc60c1f45905c02f478112021846a937400fa
qisarazu <qisarazu319@gmail.com>
2023-03-25 09:01:46 +0900

    storybook init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
-  extends: 'next/core-web-vitals',
+  extends: ['next/core-web-vitals', 'plugin:storybook/recommended'],
   plugins: ['simple-import-sort'],
   rules: {
     'react/display-name': 'off',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
   },
+  ignorePatterns: ['!.storybook'],
 };
